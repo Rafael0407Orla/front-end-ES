8d9b1ead8c4db8ca6f578687e226da37518a015d
victordz-dev <164812973+victordz-dev@users.noreply.github.com>
2024-04-28 15:23:18 -0300

    aprimorando o cupom de desconto

---
+++ srciptProductPage.js
@@ -0,0 +1,59 @@
+var productValue = 22.00
+
+function comprar() {
+    alert('Produto adicionado  ao carrinho com sucesso!');
+    document.getElementById('cupomUser').value = '';
+    document.getElementById('numberSelect').value = '0';
+    getValue()
+}
+
+function getValue() {
+    var selectElement = document.getElementById('numberSelect').value;
+    const multiplicatedPrice = productValue * selectElement;
+    document.querySelector('#dynamically-price').textContent = multiplicatedPrice;
+    applyCupom(applyCupom.disabled = false)
+}
+
+function applyCupom() {
+    var cupom = document.querySelector('#cupomUser').value;
+    var status = document.getElementById('statusCupom');
+
+    if (!applyCupom.disabled) {
+        if (cupom === 'FIAP2024') {
+            var preco = parseInt(document.getElementById('dynamically-price').textContent);
+            var preco = preco - (preco / 100 * 10);
+            document.getElementById('dynamically-price').textContent = preco;
+            status.textContent = 'Cupom Aplicado!(10% de desconto)';
+            status.style.color = "green";
+            applyCupom.disabled = true
+        }
+
+        else if (cupom.trim() != '') {
+            status.textContent = 'Cupom Inválido!'
+            status.style.color = "red"
+            applyCupom.disabled = false
+        }
+        else {
+            status.textContent = ''
+        }
+    }
+    else {
+        status.textContent = 'Cupom já aplicado!'
+    }}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const commentForm = document.getElementById('comment-form');
+    const commentInput = document.getElementById('comment-input');
+    const commentList = document.querySelector('.comment-list');
+
+    commentForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const commentText = commentInput.value.trim();
+        if (commentText !== '') {
+            const newComment = document.createElement('li');
+            newComment.textContent = commentText;
+            commentList.appendChild(newComment);
+            commentInput.value = '';
+        }
+    });
+});
