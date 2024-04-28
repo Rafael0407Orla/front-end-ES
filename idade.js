e82f546fb04b54626032be577f20395fa6580779
rafael <rafael@orla.tech>
2024-04-28 12:48:41 -0300

    refine: added some gap before show up the alert

--- idade.js
+++ idade.js
@@ -1,11 +1,16 @@
-var idade = parseInt(prompt("Qual a sua idade?"));
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(() => {
+        var idade = parseInt(prompt("Qual a sua idade?"));
 
-if (idade >= 18) {
-    alert("Você é maior de idade!");
-}
-else if (idade < 18) {
-    window.location.href = "https://google.com";
-} else {
-    alert("Erro. Digite novamente");
-    window.location.href = "index.html";
-}
+        if (idade >= 18) {
+            alert("Você é maior de idade!");
+        }
+        else if (idade < 18) {
+            alert("Desculpe, você é menor de idade!");
+            window.location.href = "https://google.com";
+        } else {
+            alert("Erro. Digite novamente");
+            window.location.href = "index.html";
+        }
+    }, 1000);
+});
